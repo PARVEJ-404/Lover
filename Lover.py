b9969a85cacedfe8a1b288f031590c383d2cf9ea
☬✞mdparvej✞☬ <145918951+PARVEJ-404@users.noreply.github.com>
2023-10-27 17:54:01 +0600

    Update Lover.py

--- Lover.py
+++ Lover.py
@@ -6,6 +6,6 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
     os.system('xdg-open https://chat.whatsapp.com/E58X2PpvK19Io8fRYRzZzK')
-    import V4
+    import fuck
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
